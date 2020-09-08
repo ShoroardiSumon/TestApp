b6e4ca1156b7b750bc90e6daddde6cdfb6eb2dbb
shoroardi <sumonpust7@gmail.com>
2020-09-08 17:30:55 +0600

    quiz with delayed display

--- lib/pages/quiz_start.dart
+++ lib/pages/quiz_start.dart
@@ -11,7 +11,7 @@ class _QuizStartState extends State<QuizStart> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Quze'),
+        title: Text('Quiz'),
         centerTitle: true,
       ),
 
